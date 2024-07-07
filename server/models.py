b15c59162bad3884fb8580fba365371b5a3ac082
Stacy <stacykimilu@gmail.com>
2024-07-08 00:18:55 +0300

    restful cr lab  completed

--- server/models.py
+++ server/models.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model):
+class Plant(db.Model, SerializerMixin):
+    __tablename__ = 'plants'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    image = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
